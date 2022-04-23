25b095f084e12ce02f470699d9978c535c363249
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-04-23 08:21:49 +0200

    Update palindrome.kt

--- Palindrome/palindrome.kt
+++ Palindrome/palindrome.kt
@@ -1,13 +1,11 @@
-fun main() {
-    string correct = "racecar";
-    string incorrect = "Farrari";
-    println(Palindrome(word: correct).isPalindrome());
-    println(Palindrome(word: incorrect).isPalindrome());
-}
-
 class Palindrome(val word: String) {
     fun isPalindrome(): Boolean {
         val wordReversed = word.reversed()
         return word == wordReversed
     }
 }   
+
+fun main() {
+    val word = "racecar"
+    println(Palindrome(word: word).isPalindrome())
+}
